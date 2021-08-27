d7fe6c48ec88680e3d85b38f789998f06c48b64c
ALAM-97 <alexamara97@gmail.com>
2021-08-27 13:30:45 +0200

    FIX - Nomi Inseriti in Lista

--- snack 3/js/script.js
+++ snack 3/js/script.js
@@ -3,11 +3,13 @@
 
 var nomi = ["Giorgio" , "Massimo", "Pietro", "Alessandro", "Pippo", "Angelo"];
 var cognomi = ["Rossi", "Verdi", "Bianchi", "Arancioni", "Viola", "Neri"];
+var invitati = [];
 
 for (i = 0; i < 3; i++) {
-    var nomeCasuale = Math.floor(Math.random(nomi) * 6);
-    var cognomeCasuale = Math.floor(Math.random(cognomi) * 6);
-    var nomiFinali = nomi[nomeCasuale] + " " + cognomi[cognomeCasuale];
-
-    console.log(nomiFinali);
+    var indiceNome = Math.floor(Math.random(nomi) * 6);
+    var indiceCognome = Math.floor(Math.random(cognomi) * 6);
+    var nomiFinali = nomi[indiceNome] + " " + cognomi[indiceCognome];
+    invitati.push(nomiFinali);
 }
+
+console.log(invitati)
