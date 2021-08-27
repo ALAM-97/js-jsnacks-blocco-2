5ff7fbd3a8f8891ff4d3bea9e2157b49b4a3b8d1
ALAM-97 <alexamara97@gmail.com>
2021-08-27 12:35:09 +0200

    FIX - Snack 2 (else if)

--- snack 2/js/script.js
+++ snack 2/js/script.js
@@ -2,13 +2,13 @@
 // Se è pari stampa il numero.
 // Se è dispari stampa il numero successivo.
 
-var userNumbers = parseInt(prompt('Inserisci numero'));
-    while (isNaN(userNumbers)) {
-        userNumbers = parseInt(prompt('ATTENZIONE! Inserisci un numero'))
+var userNumber = parseInt(prompt('Inserisci numero'));
+    while (isNaN(userNumber)) {
+        userNumber = parseInt(prompt('ATTENZIONE! Inserisci un numero'))
     }
 
-if (userNumbers % 2 == 0) {
-    console.log(userNumbers) 
-} else if (userNumbers % 2 == 1) {
-    console.log(userNumbers + 1);
+if (userNumber % 2 == 0) {
+    console.log(userNumber) 
+} else {
+    console.log(userNumber + 1);
 }
