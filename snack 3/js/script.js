6b0f52048083e7a4df2e6d6362ef33712dc4818f
ALAM-97 <alexamara97@gmail.com>
2021-08-27 13:21:51 +0200

    Snack 3 Completato (nomi NON in una lista)

--- snack 3/js/script.js
+++ snack 3/js/script.js
@@ -3,14 +3,11 @@
 
 var nomi = ["Giorgio" , "Massimo", "Pietro", "Alessandro", "Pippo", "Angelo"];
 var cognomi = ["Rossi", "Verdi", "Bianchi", "Arancioni", "Viola", "Neri"];
-var invitati = [];
 
 for (i = 0; i < 3; i++) {
     var nomeCasuale = Math.floor(Math.random(nomi) * 6);
     var cognomeCasuale = Math.floor(Math.random(cognomi) * 6);
-    var nomiFinali = nomeCasuale + " " + cognomeCasuale;
-    
+    var nomiFinali = nomi[nomeCasuale] + " " + cognomi[cognomeCasuale];
+
     console.log(nomiFinali);
 }
-
-// console.log(nomiFinali);
