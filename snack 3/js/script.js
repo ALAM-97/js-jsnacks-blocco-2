f59c186356ffa7035a611da01b1c98048e1a3458
ALAM-97 <alexamara97@gmail.com>
2021-08-27 13:12:34 +0200

    Snack 3 - Stampa Solo Numeri

---
+++ snack 3/js/script.js
@@ -0,0 +1,16 @@
+// Generatore di “nomi" e "cognomi” casuali: prendendo una lista di nomi e una
+// lista di cognomi, Gatsby vuole generare una falsa lista di 3 invitati.
+
+var nomi = ["Giorgio" , "Massimo", "Pietro", "Alessandro", "Pippo", "Angelo"];
+var cognomi = ["Rossi", "Verdi", "Bianchi", "Arancioni", "Viola", "Neri"];
+var invitati = [];
+
+for (i = 0; i < 3; i++) {
+    var nomeCasuale = Math.floor(Math.random(nomi) * 6);
+    var cognomeCasuale = Math.floor(Math.random(cognomi) * 6);
+    var nomiFinali = nomeCasuale + " " + cognomeCasuale;
+    
+    console.log(nomiFinali);
+}
+
+// console.log(nomiFinali);
